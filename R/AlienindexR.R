4c96fb47ef1f934b3906e5e997b7a78f07c7e9fd
Xiao Chen <seanchen607@gmail.com>
2023-04-17 15:45:25 +0800

    Add files via upload

--- R/AlienindexR.R
+++ R/AlienindexR.R
@@ -11,13 +11,13 @@
 ##' @param blast_output_donor Input file with 12 columns in tab-delimited format
 ##' @param blast_output_recipient Input file with 12 columns in tab-delimited format
 ##' @return dataframe
-##' @importFrom stats complete.cases
+##' @importFrom stats complete.cases 
 ##' @export 
 ##' @author Xiao Chen
 ##' @references 1. X Chen, L Yang. Manuscript in preparation.
 ##' @examples
-##' test_donor <- read.table(gzfile(system.file("extdata", c("test_0.tab", package = "AlienindexR")), header=TRUE, sep="\t")
-##' test_recipient <- read.table(gzfile(system.file("extdata", c("test_1.tab", package = "AlienindexR")), header=TRUE, sep="\t")
+##' test_donor <- read.table(gzfile(system.file("extdata", "test_0.txt.gz", package = "AlienindexR")), header=FALSE, sep="\t")
+##' test_recipient <- read.table(gzfile(system.file("extdata", "test_1.txt.gz", package = "AlienindexR")), header=FALSE, sep="\t")
 ##' AlienindexR(test_donor, test_recipient)
 
 ## Main
@@ -36,15 +36,15 @@ AlienindexR <- function(blast_output_donor,
     AI.df <- data.frame()
     if (length(shared.genes) > 0) {
         for (gene in shared.genes) {
-			donor.sub = donor[donor$qseqid == gene,,drop=F]
-			recipient.sub = recipient[recipient$qseqid == gene,,drop=F]
-			best_recipient_evalue = min(recipient.sub$evalue)
-			best_donor_evalue = min(donor.sub$evalue)
-			AI = log(best_recipient_evalue + 1e-200) - log(best_donor_evalue + 1e-200)
-			AI.df.sub = as.data.frame(list(AI))
-			colnames(AI.df.sub) = "Alien.Index"
-			rownames(AI.df.sub) = gene
-			AI.df = rbind(AI.df, AI.df.sub)
+            donor.sub = donor[donor$qseqid == gene,,drop=F]
+            recipient.sub = recipient[recipient$qseqid == gene,,drop=F]
+            best_recipient_evalue = min(recipient.sub$evalue)
+            best_donor_evalue = min(donor.sub$evalue)
+            AI = log(best_recipient_evalue + 1e-200) - log(best_donor_evalue + 1e-200)
+            AI.df.sub = as.data.frame(list(AI))
+            colnames(AI.df.sub) = "Alien.Index"
+            rownames(AI.df.sub) = gene
+            AI.df = rbind(AI.df, AI.df.sub)
         }
     } else {
         message("No shared homologs between donors and recipients!")
